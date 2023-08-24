06e352ba4458aff719b20741a7a7c063318fa568
Estelle Doriot <estelle.doriot@gmail.com>
2023-08-24 17:45:26 +0200

    initialisation du projet

---
+++ pong.py
@@ -0,0 +1,106 @@
+"""Jeu de Pong (1 ou 2 joueurs)
+Il faut rattrapper la balle avec la raquette
+"""
+
+import pygame
+
+from elements import Balle, Raquette, RaquetteAutomatique, ScoreBoard
+
+# TODO Ajouter le menu pour choisir de jouer à 1 ou 2 joueurs
+# TODO Améliorer le mouvement de la raquette automatique
+# TODO Corriger le bug de la balle coincée
+# TODO Ajouter un timer
+# TODO Ajouter une fin du jeu
+
+
+class Pong:
+    """gestion du jeu de Pong"""
+
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.largeur: int = 1280
+        self.hauteur: int = 900
+        self.fenetre: pygame.Surface = pygame.display.set_mode(
+            (self.largeur, self.hauteur)
+        )
+        pygame.display.set_caption("Pong")
+        self.clock = pygame.time.Clock()
+
+        self.balle: Balle = Balle(30)
+        self.raquette_gauche: Raquette = Raquette(
+            "gauche", pygame.Color(0, 0, 255), pygame.K_a, pygame.K_q
+        )
+        self.raquette_droite: Raquette = Raquette(
+            "droite", pygame.Color(255, 0, 0), pygame.K_p, pygame.K_m
+        )
+
+        self.scores: ScoreBoard = ScoreBoard()
+
+    def tour(self) -> None:
+        """Un tour du jeu"""
+        # point gagné
+        if self.balle.marque_joueur_droite():
+            self.scores.point_joueur_droit()
+            self.balle.relance()
+        if self.balle.marque_joueur_gauche():
+            self.scores.point_joueur_gauche()
+            self.balle.relance()
+
+        # rebond sur une raquette
+        if pygame.sprite.collide_rect(
+            self.balle, self.raquette_gauche
+        ) or pygame.sprite.collide_rect(self.balle, self.raquette_droite):
+            self.balle.rebond_vertical()
+
+        # rebond sur un mur
+        if self.balle.touche_murs():
+            self.balle.rebond_horizontal()
+
+        # mouvements des raquettes
+        self.raquette_gauche.deplacement(self.balle.hauteur)
+        self.raquette_droite.deplacement(self.balle.hauteur)
+
+        # mouvement de la balle
+        self.balle.avance()
+
+    def affichage(self) -> None:
+        """Affichage des éléments du jeu de Pong"""
+        blanc = pygame.Color(255, 255, 255)
+        noir = pygame.Color(0, 0, 0)
+        # décors
+        self.fenetre.fill(noir)
+        pygame.draw.line(
+            self.fenetre,
+            blanc,
+            (self.largeur // 2, 0),
+            (self.largeur // 2, self.hauteur),
+            2,
+        )
+        pygame.draw.circle(
+            self.fenetre, blanc, (self.largeur // 2, self.hauteur // 2), 80, 2
+        )
+        self.raquette_gauche.draw()
+        self.raquette_droite.draw()
+        self.balle.draw()
+        self.scores.draw()
+
+    def jouer(self) -> None:
+        """Lance le jeu"""
+        while True:
+            self.tour()
+
+            # quitter
+            for evenement in pygame.event.get():
+                if evenement.type == pygame.QUIT:
+                    return
+
+            self.affichage()
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    jeu = Pong()
+    jeu.jouer()
+    pygame.quit()
